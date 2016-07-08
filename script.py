26df4244fb89278a05f486e7d2ab184105457863
Conner <cjevning@gmail.com>
2016-07-08 01:38:01 -0700

    hey look disney's net income

--- script.py
+++ script.py
@@ -1,5 +1,15 @@
 from html_stripper import strip_tags
+import re
 
-with open('./quarterly_files/NICHOLAS_FINANCIAL_INC') as file:
-	data=file.read().replace('\n', '')
-	print strip_tags(data)
+with open('./yearly_files/DISNEY_WALT_CO_10-K') as file:
+	data=file.read()
+	tag_free_string = '|'.join(strip_tags(data))
+	space_stripped = re.sub(' +',' ',tag_free_string)
+	f = open ('./yearly_files/DISNEY_WALT_CO_10-K_stripped.txt', 'w')
+	f.write(space_stripped)
+	f.close()
+	NI_index = space_stripped.find('NET INCOME')
+	new_line = space_stripped.find('\n', NI_index)
+	NI_line = space_stripped[NI_index:new_line]
+	NI = re.search(r'(\d+(?:(?:\.|\,)\d+)+)', NI_line).group(0)
+	print NI
